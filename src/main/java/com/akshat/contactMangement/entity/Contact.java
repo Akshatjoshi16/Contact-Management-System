c8b273456e45793fc5c40cb3782dba2fa884ccc9
Akshatjoshi16 <akshatjoshii16@gmail.com>
2026-04-22 23:22:42 +0530

    initial commit

---
+++ src/main/java/com/akshat/contactMangement/entity/Contact.java
@@ -0,0 +1,42 @@
+package com.akshat.contactMangement.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Long number;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+}
